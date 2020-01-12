63629cd7e23ea34a691b9360026e6db544278975
Spencer <59775524+spencerlow@users.noreply.github.com>
2020-01-11 19:14:12 -0500

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+# window = TK()
+# window.title("test")
+
+window = tk.Tk()
+window.geometry("600x600")
+window.title("Murder mystery")
+window.resizable(False, False)
+window.configure(background="blue")
+
+def first_function():
+    text = "test text here"
+    textoutput = tk.Label(window, text = text, fg="red", font=("Helvetica",16))
+    textoutput.grid(row=0,column=1)
+
+testButton = tk.Button(text = "door", command = first_function)
+testButton.grid(row=0, column=0)
+
+
+
+
+if __name__ == "__main__":
+    window.mainloop()
